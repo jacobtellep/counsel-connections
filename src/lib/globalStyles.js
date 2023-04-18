a5aae45001c5c8ff5c6b935664e17663f268dd32
Jacob Tellep <jj.tellep@gmail.com>
2023-04-17 22:08:11 -0400

    flesh out global styles

--- src/lib/globalStyles.js
+++ src/lib/globalStyles.js
@@ -1,12 +1,61 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+// TODO: analyze these styles and make sure you want all of them.
 
+const GlobalStyle = createGlobalStyle`
+  /* Reset box-sizing */
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  /* Remove default margin and padding */
+  body, h1, h2, h3, h4, p, figure, blockquote, dl, dd {
+    margin: 0;
+    padding: 0;
+  }
+
+  /* Set core root defaults */
+  :root {
+    --primary-color: #0077cc;
+  }
+
+  /* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+  ul[role='list'], ol[role='list'] {
+    list-style: none;
+  }
+
+  /* Set core body defaults */
   body {
     font-family: ${({ theme }) => theme.fontFamilies.base};
+    font-family: 'Open Sans', sans-serif;
+    line-height: 1.5;
+    min-height: 100vh;
+    color: #333;
   }
+
+  /* Remove text decoration from anchor elements */
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  /* Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  /* Remove outline on focused links and buttons */
+  a:focus,
+  button:focus {
+    outline: 2px solid var(--primary-color);
+    outline-offset: 4px;
+  }
+
+  /* Add any additional styles here */
+
 `;
 
 export default GlobalStyle;

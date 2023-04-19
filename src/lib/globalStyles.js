27693a86cf329402b819ba6cafd5d0c1c3589074
Jacob Tellep <jj.tellep@gmail.com>
2023-04-18 21:41:04 -0400

    remove global padding and margin

--- src/lib/globalStyles.js
+++ src/lib/globalStyles.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: ${({ theme }) => theme.fontFamilies.base};
+    margin: 0;
+    padding: 0;
   }
 `;
 

fe3b00f615111942cd5dd2b4f5c8f29015499895
Jacob Tellep <jj.tellep@gmail.com>
2023-04-19 17:51:24 -0400

    add asset, move props

---
+++ src/screens/Homepage/fixtures.js
@@ -0,0 +1,90 @@
+export default {
+  pageTitle: 'Home',
+  pageDescription: 'Members of Counsel Connections',
+  ogImageUrl: '',
+  homepage: {
+    jumbotron: {
+      headingText: 'Welcome to Counsel Connections',
+      imageUrl: '/assets/placeholder.jpg',
+    },
+    contentBlock: {
+      headingText: "What We've Been Up To",
+      content:
+        'Springing into action: A look at our recent achievements and upcoming initiatives',
+    },
+    timelineItems: [
+      {
+        date: 'April 5, 2023',
+        id: 1,
+        eventGuests: [
+          {
+            name: 'Justice Tim Armstead',
+          },
+          {
+            name: 'Supreme Court of Appeals of West Virginia',
+          }
+        ],
+        image: {
+          alt: 'April 5, 2023',
+          src: '/assets/april-5-event.jpeg',
+          width: 2048,
+          height: 1536,
+        },
+      },
+      {
+        date: 'February 23, 2023',
+        id: 2,
+        eventGuests: [
+          {
+            name: 'Marshall Health & The Joan C. Edwards School of Medicine',
+          },
+          {
+            name: 'West Virginia School of Osteopathic Medicine',
+          },
+        ],
+        image: {
+          alt: 'February 23, 2023',
+          src: '/assets/feb-23-event.jpeg',
+          width: 854,
+          height: 640,
+        },
+      },
+      {
+        date: 'February 23, 2023',
+        id: 3,
+        eventGuests: [
+          {
+            name: 'Marshall Health & The Joan C. Edwards School of Medicine',
+          },
+          {
+            name: 'West Virginia School of Osteopathic Medicine',
+          },
+        ],
+        image: {
+          alt: 'February 23, 2023',
+          src: '/assets/feb-23-event.jpeg',
+          width: 854,
+          height: 640,
+        },
+      },
+      {
+        date: 'February 23, 2023',
+        id: 4,
+        eventGuests: [
+          {
+            name: 'Marshall Health & The Joan C. Edwards School of Medicine',
+          },
+          {
+            name: 'West Virginia School of Osteopathic Medicine',
+          },
+        ],
+        image: {
+          alt: 'February 23, 2023',
+          src: '/assets/feb-23-event.jpeg',
+          width: 854,
+          height: 640,
+        },
+      },
+    ],
+  },
+};
